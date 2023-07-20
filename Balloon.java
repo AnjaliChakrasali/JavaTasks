0fe8d145a36f3076c820902b3e95ab4268956c52
AnjaliChakrasali <anjalikl.xworkz@gmail.com>
2023-07-20 15:07:43 +0530

    Balloon code

---
+++ Balloon.java
@@ -0,0 +1,17 @@
+class Balloon{
+	public static void main(String[] arg){
+	//details
+	String brand="Party Breeze";
+	String category="Solid";
+	String color="White,Pink";
+	byte quantity=48;
+	String design="Decoration";
+	
+	//print
+	System.out.println("Brand:" +brand);
+	System.out.println("category:" +category);
+	System.out.println("color:" +color);
+	System.out.println("quantity:" +quantity);
+	System.out.println("design:" +design);
+	}
+}
